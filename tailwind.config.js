cf0ece9a328f2afa6e835fbb4e8fe23821666657
Joseph Diaz <194326262+josephhdiazg@users.noreply.github.com>
2026-04-20 10:59:00 -0500

    build: replace jekyll-wind-v2 and postcss with jekyll-vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-    content: [
-        './_drafts/**/*.html',
-        './_includes/**/*.html',
-        './_layouts/**/*.html',
-        './_posts/*.md',
-        './*.md',
-        './*.html',
-    ],
     darkMode: 'class',
     theme: {
         extend: {
